a431d2b37aa1d72a791295a5865c7783aa33bb57
Andrea <andrearivas4cinfor@gmail.com>
2023-02-06 16:54:02 +0100

    display details from API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Photo from './modules/photo.js'
+const cards = document.querySelector('.cards');
 // class Data {
   const getData = async () => {
     const response = await fetch('https://picsum.photos/v2/list')
@@ -7,20 +8,33 @@ import Photo from './modules/photo.js'
     data.forEach(element => {
       const { id, author, width, heigth, url, download_url } = element;
     const newPhoto = new Photo(id, author, width, heigth, url, download_url )
-   const cards = document.querySelector('.cards');
-    displayPhoto(data)
+   
+   displayPhotos(data)
     });
     
   } 
   
-  // }
   getData();
   
-
   
-  async function displayPhoto(photos) {
-  photos.forEach((photo => {
-   cards.innerHTML += ``
+function displayPhotos(photos) {
+  photos.forEach((photo) => {
+   cards.innerHTML += `<div class="photo-card">
+   <p id="${photo.id}">${photo.id}</p>
+   <div class="img-container">
+     <img class="photo" src="${photo.download_url}" alt="photo">
+   </div>
+   <div class="img-footer">
+     <p class="photo-author">${photo.author}</p>
+     <button class="like-btn">&#10084;</button>
+     <p class="likes-counter">7 likes</p>
+   </div>
+   <div class="buttons">
+     <button class="card-btn comments-btn">Comments</button>
+     <button class="card-btn reserve-btn">Reserve</button>
+   </div>
+ </div>`
   })
   }
+
   
