5212ab8469a049c70628469013c38425a1e028aa
Andrea <andrearivas4cinfor@gmail.com>
2023-02-08 07:08:15 +0100

    Changes requered by CP: use await instead of then

--- src/modules/apiLikes.js
+++ src/modules/apiLikes.js
@@ -2,7 +2,8 @@ const appId = 'CADKJQ2LFa6qH5nunRws';
 const urlLikesId = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes/`;
 // -- get likes from the API to use it in the next task
 const getLikes = async () => {
-  const data = await fetch(`${urlLikesId}`).then((response) => response.json());
+  const response = await fetch(urlLikesId);
+  const data = response.json();
   return data;
 };
 
