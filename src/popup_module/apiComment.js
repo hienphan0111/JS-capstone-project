76489b032024620600406b17ad1db993ecab985e
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-02-08 18:32:06 +0700

    Add display comment feature

---
+++ src/popup_module/apiComment.js
@@ -0,0 +1,31 @@
+const appId = 'CADKJQ2LFa6qH5nunRws';
+
+const getComments = async (id) => {
+  const urlInvComments = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/comments?item_id=${id}`;
+  
+  const res = await fetch(urlInvComments);
+  const result = await res.json();
+  return result;
+};
+
+async function postComment(comment) {
+  const urlInvPost = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/comments/`;
+  
+  try {
+    const response = await fetch(urlInvPost, {
+      method: 'POST',
+      body: JSON.stringify(comment),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${response.status}`);
+    }
+    return response.ok;
+  } catch (e) {
+    throw new Error('Some thing wrong! Try again');
+  }
+}
+
+export { getComments, postComment };
