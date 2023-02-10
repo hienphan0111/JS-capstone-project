6f2b2faf3f9f8b33701314cab58f674fe17c6eba
Andrea <andrearivas4cinfor@gmail.com>
2023-02-10 08:07:48 +0100

    deployment

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import './styles.css';
 import './modules/api.js';
 import displayPhotos from './modules/render.js';
 import './modules/apiLikes.js';
-
 document.addEventListener('DOMContentLoaded', displayPhotos);
---
+++ src/popup_module/countercomment.js
@@ -0,0 +1,7 @@
+
+
+const counterComment = () => {
+  const comments = document.querySelectorAll('.comment-item').length;
+  return comments;
+};
+export default counterComment;
