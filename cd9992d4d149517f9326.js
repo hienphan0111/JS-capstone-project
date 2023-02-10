b9cc42e6356e95d53fd470dd5cac3c5ea86452c3
Andrea <andrearivas4cinfor@gmail.com>
2023-02-10 09:00:12 +0100

    Updates

---
+++ cd9992d4d149517f9326.js
@@ -0,0 +1,6 @@
+import './styles.css';
+import './modules/api.js';
+import displayPhotos from './modules/render.js';
+import './modules/apiLikes.js';
+
+document.addEventListener('DOMContentLoaded', displayPhotos);
